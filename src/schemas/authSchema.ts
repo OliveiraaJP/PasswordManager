42af46a01c902481578a144187a1f70a724694ea
Joao Paulo <oliveirajpufrj@gmail.com>
2022-07-21 16:53:53 -0300

    bugfix: auth confirmPassword schema

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -4,7 +4,7 @@ export const signupSchema = joi.object({
     user: joi.string().email().required(),
     name: joi.string().required(),
     password: joi.string().required().min(10),
-    confirmPassword: joi.ref('password')
+    confirmPassword: joi.valid(joi.ref('password')).required()
 })
 
 export const signinSchema = joi.object({
